3b7efb3a373ebd63f535485e513e9c2cc8f4e2a0
phamdung2209 <phamdung.22092003@gmail.com>
2024-05-29 10:41:38 +0700

    hidden shopify key

--- Shopify-AvadaCLI/sample-app/packages/functions/src/commands/scriptTag.js
+++ Shopify-AvadaCLI/sample-app/packages/functions/src/commands/scriptTag.js
@@ -2,14 +2,12 @@
 const Shopify = require('shopify-api-node');
 
 (async () => {
-    const shopify = new Shopify({
-        shopName: 'avada-dungpv.myshopify.com',
-        accessToken: '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'
-    });
-
-    const scripttag = await shopify.scriptTag.list();
-    console.log('scripttag', scripttag);
-
+    // const shopify = new Shopify({
+    //     shopName: 'avada-dungpv.myshopify.com',
+    //     accessToken: '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'
+    // });
+    // const scripttag = await shopify.scriptTag.list();
+    // console.log('scripttag', scripttag);
     // await shopify.scriptTag.create({
     //     event: 'onload',
     //     src: 'https://localhost:3000/scripttag/index.min.js'
